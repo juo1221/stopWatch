9460090e5107a5820b0657ee2d5b4562af30dc3c
juo <hwgiwh271@gmail.com>
2021-06-03 23:38:16 +0900

    feat: 추가 얼굴인식과 스톱워치기능연결

--- js/expressions.js
+++ js/expressions.js
@@ -1,25 +1,16 @@
+"use strict";
+import { createTime, playTime, studyTime } from "./main.js";
+const playbtn = document.querySelector(".btns-auto");
 const video = document.querySelector("#video");
 const overay = document.querySelector("#overay");
 const faceDetectorOptions = new faceapi.TinyFaceDetectorOptions({
   inputSize: 160,
 });
 
-const withFaceLandmarksTinyModel = true;
-async function detect() {
-  const results = await faceapi
-    .detectSingleFace(video, faceDetectorOptions)
-    .withFaceLandmarks(withFaceLandmarksTinyModel)
-    .withFaceExpressions();
+let studyCount2 = 0;
+let playCount2 = 0;
 
-  if (results && results.expressions) {
-    const dims = faceapi.matchDimensions(overlay, video, true);
-    const resizedResults = faceapi.resizeResults(results, dims);
-    faceapi.draw.drawDetections(overlay, resizedResults);
-    // faceapi.draw.drawFaceLandmarks(overlay, resizedResults);
-    // faceapi.draw.drawFaceExpressions(overlay, resizedResults, 0.05);
-  }
-  requestAnimationFrame(detect);
-}
+const withFaceLandmarksTinyModel = true;
 
 async function run() {
   await Promise.all([
@@ -33,4 +24,28 @@ async function run() {
 
   video.addEventListener("play", detect);
 }
-run();
+
+function detect() {
+  setTimeout(async () => {
+    const results = await faceapi
+      .detectSingleFace(video, faceDetectorOptions)
+      .withFaceLandmarks(withFaceLandmarksTinyModel)
+      .withFaceExpressions();
+
+    if (results === undefined) {
+      playCount2++;
+      createTime(playTime, playCount2);
+      requestAnimationFrame(detect);
+    } else {
+      const dims = faceapi.matchDimensions(overlay, video, true);
+      const resizedResults = faceapi.resizeResults(results, dims);
+      faceapi.draw.drawDetections(overlay, resizedResults);
+
+      studyCount2++;
+      createTime(studyTime, studyCount2);
+      requestAnimationFrame(detect);
+    }
+  }, 1000);
+}
+
+playbtn.addEventListener("click", run);
